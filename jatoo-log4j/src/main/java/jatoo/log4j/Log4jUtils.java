f3d32215f2026254ba5f73e6c8a246aa63034743
Cristian Sulea <cristian.sulea.79@gmail.com>
2014-06-24 16:42:45 +0300

    v.4.0-SNAPSHOT

---
+++ jatoo-log4j/src/main/java/jatoo/log4j/Log4jUtils.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright (C) 2014 Cristian Sulea ( http://cristian.sulea.net )
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jatoo.log4j;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.List;
+
+import org.apache.commons.io.IOUtils;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * A collection of utility methods to ease the work with Apache Log4j™ logging
+ * library.
+ * 
+ * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
+ * @version 4.1, June 24, 2014
+ */
+public class Log4jUtils {
+
+  public static final String SYSTEM_PROPERTY_LOGS_DIRECTORY = "logs.directory";
+
+  private static boolean isInitialized = false;
+
+  public static void init() {
+    init(new File(System.getProperty("user.dir")));
+  }
+
+  public static void init(File workingDirectory) {
+    init(workingDirectory, new File(System.getProperty("user.dir")));
+  }
+
+  public static void init(File workingDirectory, File appDirectory) {
+
+    if (!isInitialized) {
+      synchronized (Log4jUtils.class) {
+        if (!isInitialized) {
+
+          //
+          // ensure logs directory
+          // and add him to the system properties
+
+          File logsDirectory = new File(workingDirectory, "logs");
+          logsDirectory.mkdirs();
+
+          System.setProperty(SYSTEM_PROPERTY_LOGS_DIRECTORY, logsDirectory.getAbsolutePath());
+
+          //
+          // configure from jar
+
+          try {
+            PropertyConfigurator.configure(Log4jUtils.class.getClassLoader().getResourceAsStream("config/log4j/log4j.properties"));
+          } catch (Exception e) {
+            e.printStackTrace(System.err);
+          }
+
+          //
+          // configure from config folder
+
+          if (new File(appDirectory, "config/log4j/log4j.properties").exists()) {
+            PropertyConfigurator.configure(appDirectory.getAbsolutePath() + "/config/log4j/log4j.properties");
+          }
+
+          //
+          // load loggers from jar
+
+          try {
+
+            Enumeration<URL> loggersEnumeration = Log4jUtils.class.getClassLoader().getResources("config/log4j/loggers.list");
+
+            while (loggersEnumeration.hasMoreElements()) {
+              URL loggersURL = (URL) loggersEnumeration.nextElement();
+
+              try {
+
+                List<String> loggers = IOUtils.readLines(loggersURL.openStream());
+
+                for (String logger : loggers) {
+                  PropertyConfigurator.configure(Log4jUtils.class.getClassLoader().getResourceAsStream("config/log4j/loggers/" + logger));
+                }
+              }
+
+              catch (Exception e) {
+                e.printStackTrace(System.err);
+              }
+            }
+          }
+
+          catch (Exception e) {
+            e.printStackTrace(System.err);
+          }
+
+          //
+          // load loggers from config folder
+
+          String[] loggers = new File(appDirectory, "config/log4j/loggers/").list(new FilenameFilter() {
+            public boolean accept(File dir, String name) {
+              return name.endsWith(".properties");
+            }
+          });
+
+          if (loggers != null && loggers.length > 0) {
+            for (String logger : loggers) {
+              PropertyConfigurator.configure(appDirectory.getAbsolutePath() + "/config/log4j/loggers/" + logger);
+            }
+          }
+
+          //
+          // done
+
+          isInitialized = true;
+        }
+      }
+    }
+  }
+
+  public static boolean isInitialized() {
+    synchronized (Log4jUtils.class) {
+      return isInitialized;
+    }
+  }
+
+  public static void destroy() {
+
+    if (isInitialized) {
+      synchronized (Log4jUtils.class) {
+        if (isInitialized) {
+
+          System.clearProperty(SYSTEM_PROPERTY_LOGS_DIRECTORY);
+
+          LogManager.shutdown();
+
+          isInitialized = false;
+        }
+      }
+    }
+  }
+
+}
---
+++ jatoo-log4j/src/test/java/jatoo/log4j/Log4jUtilsTest.java
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2014 Cristian Sulea ( http://cristian.sulea.net )
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package jatoo.log4j;
+
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * {@link Log4jUtils} test class.
+ * 
+ * @author <a href="http://cristian.sulea.net" rel="author">Cristian Sulea</a>
+ * @version 1.0, June 24, 2014
+ */
+public class Log4jUtilsTest {
+
+  private static final File WORKING_DIRECTORY = new File("target");
+  private static final File APP_DIRECTORY = new File("target");
+
+  private static final File LOGS1_DIRECTORY = new File("logs");
+  private static final File LOGS2_DIRECTORY = new File(WORKING_DIRECTORY, "logs");
+
+  @BeforeClass
+  public static void beforeClass() {}
+
+  @AfterClass
+  public static void afterClass() {}
+
+  @Before
+  public void before() {
+    Assert.assertNull(System.getProperty(Log4jUtils.SYSTEM_PROPERTY_LOGS_DIRECTORY));
+  }
+
+  @After
+  public void after() throws Exception {
+
+    Log4jUtils.destroy();
+
+    Assert.assertNull(System.getProperty(Log4jUtils.SYSTEM_PROPERTY_LOGS_DIRECTORY));
+
+    FileUtils.deleteDirectory(LOGS1_DIRECTORY);
+    Assert.assertFalse(LOGS1_DIRECTORY.exists());
+
+    FileUtils.deleteDirectory(LOGS2_DIRECTORY);
+    Assert.assertFalse(LOGS2_DIRECTORY.exists());
+  }
+
+  @Test
+  public void test1() {
+
+    Log4jUtils.init();
+
+    Assert.assertNotNull(System.getProperty(Log4jUtils.SYSTEM_PROPERTY_LOGS_DIRECTORY));
+  }
+
+  @Test
+  public void test2() {
+
+    Log4jUtils.init(WORKING_DIRECTORY);
+
+    Assert.assertEquals(new File(WORKING_DIRECTORY, "logs").getAbsolutePath(), System.getProperty("logs.directory"));
+    Assert.assertTrue(new File(WORKING_DIRECTORY, "logs").exists());
+  }
+
+  @Test
+  public void test3() {
+
+    Log4jUtils.init(WORKING_DIRECTORY, APP_DIRECTORY);
+
+    Assert.assertEquals(new File(WORKING_DIRECTORY, "logs").getAbsolutePath(), System.getProperty("logs.directory"));
+    Assert.assertTrue(new File(WORKING_DIRECTORY, "logs").exists());
+  }
+
+}
